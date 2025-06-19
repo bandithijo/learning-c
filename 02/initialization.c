e2c1a516dc83dfa936e3195f3ee3cdbf14061865
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-06-20 05:41:18 +0800

    02 Introduction to Variables

---
+++ 02/initialization.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/* NOTE:
+ * %d is placeholder for decimal
+ * */
+
+int main()
+{
+	// int var = 3;
+	// var = 4;
+	// printf("%d", var);
+	
+	// int var1 = 3;
+	// int var2;
+	// var2 = var1;
+	// printf("%d", var2);
+
+	int var1, var2, var3;
+	var1 = var2 = var3 = 4;
+	printf("%d", var1);
+	printf("%d", var2);
+	printf("%d", var3);
+
+	return 0;
+}
